db74847a2f59586fbf6f8fc92b156ec6a280ba6f
Matheus Quintino <malimaquintino@gmail.com>
2024-09-04 21:21:28 -0300

    fix

--- src/main/java/com/malimaquintino/javamongo/util/MockDataGen.java
+++ src/main/java/com/malimaquintino/javamongo/util/MockDataGen.java
@@ -15,6 +15,25 @@ import java.util.Locale;
 @UtilityClass
 public class MockDataGen {
 
+    private final List<String> dataBaseNames = List.of("OTERO", "MONET", "IA_TEST", "MAGNETO", "MAGENTO", "MILLIAN",
+            "AMARO", "VIVA_BEM", "ANOMALIA", "LUDWICKING", "MAESTRIA", "ALFA", "MEGAZORD", "BRITALO", "JARDINS", "ACCAPULCO",
+            "GAUGA", "JAMAL", "WEB_MOV", "ATROVEM", "ULTRA_JOVEM", "NEO_WEB", "SPUTINICK", "FRAM", "GIRA_MUNDO", "CARD_CRED",
+            "ITA_TAX", "JINMES", "HOOK2B", "WORLD_DOMINATION", "Sinonyas", "Esxeowos", "Noisvuar", "Kozozosi", "Urnaekoy",
+            "Biufovul", "Ashiavui", "Xyozalal", "Gueonzyo", "Ricleten", "Olgotafi", "Dubruern", "Voyvonai", "VOLARE",
+            "KRIST_KROST", "MITRAL", "LOVE_MORE", "TENAK", "OLAF_MASTER", "GUINIL", "GEMINIE", "TAM", "DESTINEY",
+            "LIFE_WAY", "AIR_JETS", "KMIKAZE", "DROP_THE_BOMB", "SOND_CORE", "MUSIC_STATION", "METRO2033",
+            "SUMMER_FEST", "RAT_CAT", "FROID", "TRABUCO_DONOZOR", "MANJERICAO", "AMBROZIL", "TAMAINDO", "IOI201",
+            "NEO_MILLENIUM", "VAM_MORE", "TRALES", "MONGOLIANO", "TRIBOLINA", "KRISTAL", "MONGOLIANA", "LOVINARIA",
+            "foguete", "bandeira", "abacaxi", "animais", "sinopse", "sintaxe", "noitada", "cabra", "mesas", "porteiras",
+            "auditor", "taxistas", "biologo");
+
+    private List<String> tablesNames = List.of("CAVALO", "MAMACO", "VACALO", "TAX", "TABUCO", "GULHOTINA",
+            "DEBIT", "PAY_MORE", "FRUITAS", "XAROPE", "RATOS", "USUARIOS", "ENDERECO", "PATHS", "IMAGENS", "COISAS",
+            "NEGOCIO", "BUSINESS", "GENERO", "HOMEM_MULHER", "ENDERECO_ELETRONICO", "FABRICAS", "CORES", "FLORES",
+            "TIPOS", "VENDAS", "REPORTS", "TEXT_CHOISE", "ELEMENTOS", "GARGANTAS", "TOENS", "CONTA", "IPS", "MASAS",
+            "LOG", "SYSTEM_EMAIL", "EMAIL", "INTERNAL", "LIRAS", "SEMENTES", "TERRENO", "GEOMETRIA", "JUSTICA",
+            "CALIBRE", "DRONE", "VEICULOS", "LISTAS", "RANK");
+
     public static DatabaseInputDTO generateDatabase() {
         Faker faker = new Faker(new Locale("pt-BR"));
 
@@ -25,7 +44,7 @@ public class MockDataGen {
         }
 
         return DatabaseInputDTO.builder()
-                .name(faker.app().name().toUpperCase() + "_" + getRandomNumberUsingNextInt(0, 999999))
+                .name(randomDataBaseName().toUpperCase() + "_" + getRandomNumberUsingNextInt(0, 999999))
                 .indTechnology(randomTechnology())
                 .databaseId(faker.idNumber().ssnValid())
                 .containerId(faker.idNumber().ssnValid())
@@ -46,8 +65,8 @@ public class MockDataGen {
         }
 
         return TableInputDTO.builder()
-                .schema(faker.dog().name() + "_" + getRandomNumberUsingNextInt(0, 999999))
-                .name(faker.cat().name() + "_" + getRandomNumberUsingNextInt(0, 999999))
+                .schema(randomDataBaseName().toUpperCase() + "_" + getRandomNumberUsingNextInt(0, 999999))
+                .name(randomTableName().toUpperCase() + "_" + getRandomNumberUsingNextInt(0, 999999))
                 .comment(faker.rickAndMorty().quote())
                 .status("ACTIVE")
                 .columns(columnInputDTOList)
@@ -73,6 +92,16 @@ public class MockDataGen {
         List<String> technology = Arrays.asList("ORACLE", "MYSQL", "REDSHIFT", "MARIADB");
         SecureRandom rand = new SecureRandom();
         return technology.get(rand.nextInt(technology.size()));
+    }
+
+    public static String randomDataBaseName() {
+        SecureRandom rand = new SecureRandom();
+        return dataBaseNames.get(rand.nextInt(dataBaseNames.size()));
+    }
+
+    public static String randomTableName() {
+        SecureRandom rand = new SecureRandom();
+        return tablesNames.get(rand.nextInt(tablesNames.size()));
     }
 
     public static int getRandomNumberUsingNextInt(int min, int max) {
